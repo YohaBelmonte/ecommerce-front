36823ee042360cdac6aa2ae4cb76f3d862da11bb
Yoha Belmonte <yohabelmonte@hotmail.com>
2023-01-13 20:15:04 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ShoppingCart from "./Pages/ShoppingCart/ShoppingCart";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      Hola mundo
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Navbar />
+              <div className="mt-5">
+                <ShoppingCart />
+              </div>
+            </div>
+          }
+        ></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,14 @@
+import "./ShoppingCart.css";
+
+function ShoppingCart(){
+
+    return (
+        <div className="shop-container"> 
+        dsads
+           <div className="cart-container">
+           Hola SHOPPING
+           </div>
+        </div>
+    )
+}
+export default ShoppingCart;
