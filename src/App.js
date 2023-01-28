af237d812b595ddbc75ff7053b6a8d564ec3b333
yulianasenia <yulianasenia@gmail.com>
2023-01-28 12:16:00 -0300

    agrego seccion about us

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
 import './App.css';
-import RegisterPage from './Pages/Register/RegisterPage';
-import { Form, Button } from "react-bootstrap";
+import AboutPage from "./Pages/AboutUs/AboutUs";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoginPage from './Pages/Login/LoginPage';
-import HomePage from './Pages/Home/HomePage';
+
 
 function App() {
 
   return (
 
     <BrowserRouter >
-
       <Routes>
-        <Route path='/' element={
-          <div>
-            <div className='mt-5'>
-              <HomePage/>
-            </div>
-          </div>
-        }>
+        <Route path='/about' element={<AboutPage />}>
         </Route>
-        <Route path='/Login' element={
-          <div>
-
-            <div className='mt-5'>
-              <LoginPage />
-            </div>
-          </div>
-        }>
-        </Route>
-
-        <Route path='/Register' element={
-          <div>
-
-            <div className='mt-5'>
-              <RegisterPage />
-            </div>
-          </div>
-        }>
-        </Route>
-
-
       </Routes>
     </BrowserRouter>
   );
