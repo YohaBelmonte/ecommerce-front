6534556180a4c5c9c9de4a2e51f813f6322a3b32
Yoha Belmonte <yohabelmonte@hotmail.com>
2023-02-05 00:36:59 -0300

    d

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -3,22 +3,25 @@ import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import useHome from "../../Utils/useHome";
 import NavBarComponent from "../../components/Navbar/Navbar";
-// import PreLoader from "../../components/LoadingScreen/LoadingScreen";
+import Footer from "../../components/Footer/Footer";
 
 function HomePage() {
+
 //hook
 const { MapProduct } = useHome();
 
   return (
     //home de prueba ↓↓↓↓ para ver si andaba el router y el register , login   ↓↓↓↓  ↓↓↓↓  ↓↓↓↓  ↓↓↓↓  ↓↓↓↓  ↓↓↓↓
     <div>
-      {/* <PreLoader/> */}
       <NavBarComponent/>
       <div className="d-flex flex-column-reverse MapProductContainer">
         {MapProduct}
       </div>
+      <Footer />
     </div>
   );
 }
 
 export default HomePage;
+
+  
