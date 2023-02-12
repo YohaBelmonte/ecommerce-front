c59c9151981976558a78201f6fa432fae222376c
yulianasenia <yulianasenia@gmail.com>
2023-02-11 21:11:07 -0300

    date

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -28,19 +28,20 @@ function NavBarComponent({ size }) {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="link-navbar mt-4">
             <li className='container-link'>
-              <Link to="/" className='link-nav mx-lg-4 mt-lg-3'>INICIO</Link>
+              <Link to="/" className='link-nav mx-lg-3 mt-lg-3'>INICIO</Link>
               <Link to="/male" className='link-nav'>MASCULINO</Link>
-              <Link to="/famele" className='link-nav mx-lg-4'>FEMENINO</Link>
+              <Link to="/famele" className='link-nav mx-lg-3'>FEMENINO</Link>
               <Link to="/aboutUs" className='link-nav'>SOBRE NOSOTROS</Link>
-              <Link to="/contact" className='link-nav mx-lg-4'>CONTACTO</Link>
+              <Link to="/contact" className='link-nav mx-lg-3'>CONTACTO</Link>
               {isAdmin ? (
                 <Link to="/admin" className='link-nav'> <p className="admin-nav">ADMINISTRADOR </p></Link>
               ) : ("")}
-            <Link to="/cart" className='link-nav shopping-cart1 mx-lg-5'>MI CARRITO</Link>
+               <Link to="/admin" className='link-nav'> <p className="admin-nav">ADMINISTRADOR </p></Link>
+            <Link to="/cart" className='link-nav shopping-cart1 mx-lg-4'>MI CARRITO</Link>
             </li>
             <form>
               <input type="search" placeholder="Buscar" className="input-navbar" />
-              <Link to="/login" className='mx-lg-2 icon-nav'>
+              <Link to="/login" className='icon-nav'>
               <FaUser />
             </Link>
             <Link to="/cart " className='icon-nav'>
