5493cd7cb487f7c8eaec01598c7207a869b18ce1
yulianasenia <yulianasenia@gmail.com>
2023-02-12 16:59:22 -0300

    up date

--- src/components/ComponentHome/ComponentHome.js
+++ src/components/ComponentHome/ComponentHome.js
@@ -20,37 +20,37 @@ function ComponentHome() {
                 <div className='container-cardHome row'>
                     <div className='cards-home col-lg-4 col-md-12 col-xs-12'>
                         <div className='card-home'>
-                        <Link to="/" ><img src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/4ee3c3061fc5464b94c9af5e00a4a964_9366/Avryn_Shoes_Gris_HP5974_01_standard.jpg" className='img-cardHome' /></Link>
+                        <Link to="/" ><img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSdNB1C8OC04OzIIu_uFWU5IcjNtDgER_1VC67DzQt1dUwx_J4UaKkXe6-l8C4BFxyu1bQQAx-LJE2VqOP9hCuMV3Oa5jGf" className='img-cardHome' /></Link>
                             <div class="card-header">
-                                <h4 class="title">AVRYN SHOES</h4>
-                                <h4 class="price">$13300</h4>
+                                <h4 class="title">STREETWEAR</h4>
+                                <h4 class="price">$55500</h4>
                             </div>
                             <div class="card-body">
-                                <p>Las próximas estrellas de tu colección de zapatillas. Estos tenis estilo running agregan un toque deportivo a cualquier atuendo, desde jeans desgastados hasta sastrería.</p>
+                                <p>AIR JORDAN 1 MID | BEIGE WHITE RED. Es una zapatilla del año 2022 que forma parte de la colección Jordan 1. Viene en una combinación de colores White Onyx/Cardinal Red/Light Curry, construido con revestimientos de cuero beige.</p>
                             </div>
                         </div>
                     </div>
                     <div className='cards-home col-lg-4 col-md-12 col-xs-12'>
                         <div className='card-home'>
-                        <Link to="/" > <img src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/9fa15fcba90349b99883af5e00a4c3cb_9366/Avryn_Shoes_Gris_HP5974_03_standard.jpg" className='img-cardHome' /></Link>
+                        <Link to="/" > <img src="https://woker.vtexassets.com/arquivos/ids/292438-800-800?v=637945269639330000&width=800&height=800&aspect=true" className='img-cardHome' /></Link>
                             <div class="card-header">
-                                <h4 class="title">AVRYN SHOES</h4>
-                                <h4 class="price">$13300</h4>
+                                <h4 class="title">PARLEY ADIDAS</h4>
+                                <h4 class="price">$48300</h4>
                             </div>
                             <div class="card-body">
-                                <p>La parte superior de malla aireada tiene un estilo moderno en capas, mientras que la suela gruesa combina lo mejor de la tecnología de JYMD Shoes. </p>
+                                <p>El cambio es posible cuando trabajamos juntos. Es por eso que se asociaron Parley for the Oceans en estos zapatos junto a adidas Forum, como un recordatorio de que todos tenemos un papel que desempeñar para ayudar a acabar con los desechos plásticos. </p>
                             </div>
                         </div>
                     </div>
                     <div className='cards-home col-lg-4 col-md-12 col-xs-12'>
                         <div className='card-home'>
-                        <Link to="/" > <img src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/cba142818f944575b352af5e00a4d822_9366/Avryn_Shoes_Gris_HP5974_05_standard.jpg" className='img-cardHome' /></Link>
+                        <Link to="/" > <img src="https://sothebys-md.brightspotcdn.com/53/92/d28052c64b13b38626ff2b63b049/louis-vuitton-x-nike-air-force-1-low-orange.jpg" className='img-cardHome' /></Link>
                             <div class="card-header">
-                                <h4 class="title">AVRYN SHOES</h4>
+                                <h4 class="title">LUXURY</h4>
                                 <h4 class="price">$13300</h4>
                             </div>
                             <div class="card-body">
-                                <p>Fabricada con una serie de materiales reciclados. Este producto representa solo una de nuestras soluciones para ayudar a acabar con los residuos plásticos.</p>
+                                <p>Para Primavera-Verano 2022, Virgil Abloh colaboró ​​con Nike en Air Force 1 a medida, fusionando los códigos clásicos de la zapatilla con la insignia y los materiales de Louis Vuitton en homenaje a la cultura hip-hop que lo moldeó.</p>
                             </div>
                         </div>
                     </div>
