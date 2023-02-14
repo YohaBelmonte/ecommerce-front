ed72a819be13eba0f0ae0d6f2a1fd3d3975b04d5
Yoha Belmonte <yohabelmonte@hotmail.com>
2023-02-14 18:25:21 -0300

    footer agregado en seccion admin

--- src/Pages/Admin/Productos.js
+++ src/Pages/Admin/Productos.js
@@ -1,19 +1,20 @@
 import AdminNavbar from "../../components/Navbar/Navbar";
-import Sidebar from "../Admin/Sidebar/Sidebar"; 
-import "./admin.css"
+import Sidebar from "../Admin/Sidebar/Sidebar";
+import "./admin.css";
 import TablaProductos from "../../components/TablaProductos/TablaProductos";
+import Footer from "../../components/Footer/Footer";
 
 function Products() {
-    return (
-        <div>
-<AdminNavbar />
-<div className="d-flex m-0">
-<Sidebar/>
-<TablaProductos/>
-</div>
-
-        </div>
-    );
+  return (
+    <div>
+      <AdminNavbar />
+      <div className="d-flex m-0">
+        <Sidebar />
+        <TablaProductos />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Products;
--- src/Pages/Admin/Usuarios.js
+++ src/Pages/Admin/Usuarios.js
@@ -1,20 +1,20 @@
 import NavBarComponent from "../../components/Navbar/Navbar";
 import Sidebar from "../Admin/Sidebar/Sidebar";
 import TablaUsuarios from "../../components/TablaUsuarios/TablaUsuarios";
-import "./admin.css"
+import Footer from "../../components/Footer/Footer";
+import "./admin.css";
 
 function Usuarios() {
-    return (
-        <div>
-            <NavBarComponent />
-            <div className="d-flex m-0">
-                <Sidebar />
-                <TablaUsuarios/>
-                
-            </div>
-
-        </div>
-    );
+  return (
+    <div>
+      <NavBarComponent />
+      <div className="d-flex m-0">
+        <Sidebar />
+        <TablaUsuarios />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Usuarios;
