608674fe83eaec79679ba54d611df776f4617a54
Yoha Belmonte <yohabelmonte@hotmail.com>
2023-03-22 09:09:01 -0300

    update

--- src/components/ContactInputs/ContactInput.js
+++ src/components/ContactInputs/ContactInput.js
@@ -1,4 +1,4 @@
-import './ContactInput.css';
+import "./ContactInput.css";
 import { Form, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
@@ -6,89 +6,77 @@ import axios from "axios";
 import React from "react";
 
 function ContactInput() {
-    const [form, setForm] = useState({});
+  const [form, setForm] = useState({});
 
-    var url = "http://localhost:4000/api";
+  var url = "http://localhost:4000/api";
 
-    function OnChange(e) {
-        const { name, value, } = e.target;
-        const response = {
-            ...form, [name]: value,
-        };
-        setForm(response);
+  function OnChange(e) {
+    const { name, value } = e.target;
+    const response = {
+      ...form,
+      [name]: value,
+    };
+    setForm(response);
+  }
+
+  async function Send() {
+    try {
+      const token = localStorage.getItem("token") ?? "";
+      const headers = { "x-auth-token": token };
+      const response = await axios.post(`${url}/email`, form, { headers });
+      alert("Mail enviado con exito");
+      // window.location.href = "/"
+    } catch (error) {
+      console.error(error);
     }
+  }
 
+  return (
+    <div className="d-flex justify-content-center ContactInput py-5">
+      <Form className="col-lg-6 col-sm-6 ">
+        <p className="text-center">Contacto</p>
 
-    async function Send() {
+        <Form.Group className="my-5">
+          <Form.Control
+            className="border-bottom border-0 bg-light rounded-0 fs-5 "
+            type="email"
+            name="email"
+            placeholder="email"
+            onChange={OnChange}
+          />
+        </Form.Group>
+        <Form.Group className="my-5">
+          <Form.Control
+            className="border-bottom border-0 bg-light rounded-0 fs-5 "
+            type="text"
+            name="title"
+            placeholder="Titulo"
+            onChange={OnChange}
+          />
+        </Form.Group>
+        <Form.Group className="my-5">
+          <Form.Control
+            className="border-bottom border-0 bg-light rounded-0 fs-5"
+            type="text"
+            name="description"
+            placeholder="Mensaje"
+            onChange={OnChange}
+            as="textarea"
+          />
+        </Form.Group>
 
-        try {
-            
-            
-            const token = localStorage.getItem("token") ?? "";
-            const headers = { "x-auth-token": token };
-            const response = await axios.post(`${url}/email`, form, {headers});
-            alert("Mail enviado con exito")
-            // window.location.href = "/"
-
-        } catch (error) {
-
-            console.error(error)
-        }
-
-    }
-
-
-
-    return (
-        <div className="d-flex justify-content-center ContactInput py-5">
-            <Form className="col-lg-6 col-sm-6 ">
-                <p className="text-center">Contacto</p>
-               
-
-                <Form.Group className="my-5">
-                    <Form.Control
-                        className="border-bottom border-0 bg-light rounded-0 fs-5 "
-                        type="email"
-                        name="email"
-                        placeholder="email"
-                        onChange={OnChange}
-                    />
-                </Form.Group>
-                <Form.Group className="my-5">
-                    <Form.Control
-                        className="border-bottom border-0 bg-light rounded-0 fs-5 "
-                        type="text"
-                        name="title"
-                        placeholder="Titulo"
-                        onChange={OnChange}
-                    />
-                </Form.Group>
-                <Form.Group className="my-5">
-                    <Form.Control
-                        className="border-bottom border-0 bg-light rounded-0 fs-5"
-                        type="text"
-                        name="description"
-                        placeholder="Mensaje"
-                        onChange={OnChange}
-                        as="textarea"
-                    />
-                </Form.Group>
-               
-
-                <div className="d-flex justify-content-center ">
-                    <Button
-                        className=" btn  btn-dark w-100  rounded-0 fs-5"
-                        variant="primary"
-                        onClick={Send}
-                    >
-                        Enviar
-                    </Button>
-                </div>
-            </Form>
-
-
+        <div className="d-flex justify-content-center ">
+          <Button
+            className=" btn  btn-dark w-100  rounded-0 fs-5"
+            variant="primary"
+            onClick={Send}
+          >
+            Enviar
+          </Button>
         </div>
-    );
+      </Form>
+    </div>
+  );
 }
 
 export default ContactInput;
--- src/components/CreditCard/PaymentForms.js
+++ src/components/CreditCard/PaymentForms.js
@@ -1,116 +1,116 @@
-import React, { useState } from 'react'
-import Cards from 'react-credit-cards'
-import 'react-credit-cards/es/styles-compiled.css'
+import React, { useState } from "react";
+import Cards from "react-credit-cards";
+import "react-credit-cards/es/styles-compiled.css";
 import "./PaymentForm.css";
 import { Link } from "react-router-dom";
 
 const PaymentForms = () => {
+  const [state, setState] = useState({
+    number: "",
+    name: "",
+    expiry: "",
+    cvc: "",
+    focus: "",
+  });
 
-    const [state, setState] = useState({
-        number: "",
-        name: "",
-        expiry: "",
-        cvc: "",
-        focus: ""
-    })
+  const handleInputChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleInputChange = (e) => {
-        setState({
-            ...state,
-            [e.target.name] : e.target.value
-        })
-    }
+  const handleFocusChange = (e) => {
+    setState({
+      ...state,
+      focus: e.target.name,
+    });
+  };
 
-    const handleFocusChange = (e) => {
-        setState({
-            ...state,
-            focus : e.target.name
-        })
-    }
+  return (
+    <div className="card w-100 h-100">
+      <div className="card-body">
+        <Cards
+          number={state.number}
+          name={state.name}
+          expiry={state.expiry}
+          cvc={state.cvc}
+          focused={state.focus}
+        />
+        <form className=" w-75 ">
+          <div className="form-group">
+            <label htmlFor="name">Email</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              maxLength="30"
+              className="form-control w-100"
+              placeholder="Ingresar email"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="number">Numero de la tarjeta</label>
+            <input
+              type="text"
+              name="number"
+              id="number"
+              maxLength="16"
+              className="form-control w-100 "
+              onChange={handleInputChange}
+              onFocus={handleFocusChange}
+              placeholder="Ingresar el numero de la tarjeta"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="name">Nombre</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              maxLength="30"
+              className="form-control w-100"
+              onChange={handleInputChange}
+              onFocus={handleFocusChange}
+              placeholder="Ingresar Nombre completo"
+            />
+          </div>
 
-    return (
-        <div className="card w-100 h-100">
-            <div className="card-body">
-
-                <Cards 
-                    number={state.number}
-                    name={state.name}
-                    expiry={state.expiry}
-                    cvc={state.cvc}
-                    focused={state.focus}
-                /> 
-                <form className=' w-75 '>
-                <div className="form-group">
-                        <label htmlFor="name">Email</label>
-                        <input
-                            type="text"
-                            name="name"
-                            id="name"
-                            maxLength="30"
-                            className="form-control w-100"
-                            placeholder='Ingresar email'
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="number">Numero de la tarjeta</label>
-                        <input
-                            type="text"
-                            name="number"
-                            id="number"
-                            maxLength="16"
-                            className="form-control w-100 "
-                            onChange={handleInputChange}
-                            onFocus={handleFocusChange}
-                            placeholder="Ingresar el numero de la tarjeta"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="name">Nombre</label>
-                        <input
-                            type="text"
-                            name="name"
-                            id="name"
-                            maxLength="30"
-                            className="form-control w-100"
-                            onChange={handleInputChange}
-                            onFocus={handleFocusChange}
-                            placeholder="Ingresar Nombre completo"
-                        />
-                    </div>
-                    
-                    <div className="form-row">
-                        <div className="form-group col-md-6">
-                            <label htmlFor="expiry">Fecha de expiración</label>
-                            <input
-                                type="text"
-                                name="expiry"
-                                id="expiry"
-                                maxLength="4"
-                                className="form-control"
-                                onChange={handleInputChange}
-                                onFocus={handleFocusChange}
-                                placeholder="Ingresar fecha de expiracion"
-                            />
-                        </div>
-                        <div className="form-group col-md-6">
-                            <label htmlFor="cvc">CVC</label>
-                            <input
-                                type="text"
-                                name="cvc"
-                                id="cvc"
-                                maxLength="4"
-                                className="form-control"
-                                onChange={handleInputChange}
-                                onFocus={handleFocusChange}
-                                placeholder="Ingresar CVC"
-                            />
-                        </div>
-                    </div>
-                    <button type="button" className="btnPay">Pagar</button>
-                </form>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label htmlFor="expiry">Fecha de expiración</label>
+              <input
+                type="text"
+                name="expiry"
+                id="expiry"
+                maxLength="4"
+                className="form-control"
+                onChange={handleInputChange}
+                onFocus={handleFocusChange}
+                placeholder="Ingresar fecha de expiracion"
+              />
             </div>
-        </div>
-    )
-}
+            <div className="form-group col-md-6">
+              <label htmlFor="cvc">CVC</label>
+              <input
+                type="text"
+                name="cvc"
+                id="cvc"
+                maxLength="4"
+                className="form-control"
+                onChange={handleInputChange}
+                onFocus={handleFocusChange}
+                placeholder="Ingresar CVC"
+              />
+            </div>
+          </div>
+          <button type="button" className="btnPay">
+            Pagar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default PaymentForms
+export default PaymentForms;
